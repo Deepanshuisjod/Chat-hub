8e31ac9fa6f8d8bacb716daa4286dfd7684a48d6
Deepanshuisjod <deepanshudb1@gmail.com>
2024-02-19 18:00:02 +0530

    add/modify landingpage directory
    contains LandingPage.js & LandingPage.module.css which is rendered in App.js . Modification is : created a nav bar and style it

---
+++ src/landingpage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './LandingPage.module.css'
+export default function LandingPage() {
+  return (
+    <body>
+        <header>
+          <div id="chat-hub">Chat Hub</div>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Sign In</a>
+            </nav>
+        </header>
+    </body>
+  );
+}
