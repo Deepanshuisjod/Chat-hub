b6d9c82162ab486980453e42a00f35ddc23c37bf
Deepanshuisjod <deepanshudb1@gmail.com>
2024-02-20 16:30:02 +0530

    modify src/landingpage/LandingPage.js

--- src/landingpage/LandingPage.js
+++ src/landingpage/LandingPage.js
@@ -1,16 +1,34 @@
 import React from "react";
-import './LandingPage.module.css'
+import styles from './LandingPage.module.css'
 export default function LandingPage() {
   return (
     <body>
         <header>
-          <div id="chat-hub">Chat Hub</div>
+          <div>ChatHub</div>
             <nav>
                 <a href="#">Home</a>
                 <a href="#">About</a>
                 <a href="#">Sign In</a>
             </nav>
         </header>
+        <p className={styles.qlp}>Chat seamlessly, connect effortlessly – your perfect conversation starts here</p>
+        <form>
+          <div className={styles.username}>
+            <label>Username</label>
+            <input/>
+          </div>
+          <div className={styles.list}>
+            <label>Your Gender : </label>
+            <label className={styles.gender}>
+              <input type="radio" name="gender" className={styles.radio} />
+              Male
+            </label>
+            <label className={styles.gender}>
+              <input type="radio" name="gender" className={styles.radio}/>
+              Female
+            </label>
+          </div>
+        </form>
     </body>
   );
 }
